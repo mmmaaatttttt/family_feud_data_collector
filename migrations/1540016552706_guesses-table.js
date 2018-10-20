c302a0e89f5cf7f60bd7b42d4957eb6169a65ddd
Matt Lane <lane.matthew@gmail.com>
2018-10-20 09:44:04 -0700

    making guesses table

---
+++ migrations/1540016552706_guesses-table.js
@@ -0,0 +1,19 @@
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createTable("answers", {
+    id: "id",
+    question_id: {
+      type: "integer",
+      notNull: true,
+      references: '"questions"',
+      onDelete: "cascade"
+    },
+    text: { type: "text", notNull: true },
+    matching_answer_id: {
+      type: "integer",
+      references: '"answers"',
+      onDelete: "cascade"
+    },
+  });
+};
