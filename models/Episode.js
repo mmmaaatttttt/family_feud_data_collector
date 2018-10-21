556f78e557c98a07bc34595b52800b39fced4057
Matt Lane <lane.matthew@gmail.com>
2018-10-20 21:13:56 -0700

    removing title as column, it can be inferred

--- models/Episode.js
+++ models/Episode.js
@@ -15,7 +15,6 @@ class Episode {
     this.episode_number = episode_number;
     this.season = season;
     this.air_date = air_date;
-    this.title = title;
     this.first_team_id = first_team_id;
     this.second_team_id = second_team_id;
   }
@@ -26,21 +25,25 @@ class Episode {
         episode_number,
         season,
         air_date,
-        title,
         first_team_id,
         second_team_id
       )
-      VALUES ($1, $2, $3, $4, $5, $6) RETURNING *
+      VALUES ($1, $2, $3, $4, $5) RETURNING *
     `, [
       dataObj.episode_number,
       dataObj.season,
       dataObj.air_date,
-      dataObj.title,
       dataObj.first_team_id,
       dataObj.second_team_id
     ]);
     let newEpisode = result.rows[0];
     return new Episode(newEpisode);
+  }
+
+  async get title() {
+    let team1 = await Team.get(this.first_team_id);
+    let team2 = await Team.get(this.second_team_id);
+    return `${team1.name} vs. ${team2.name}`;
   }
 }
 
