8392361691a3c9cb1a621af93ac352a6dcaf95da
Matt Lane <lane.matthew@gmail.com>
2018-10-20 19:50:23 -0700

    adding index.js

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Answer = require("./Answer");
+const Episode = require("./Episode");
+const Guess = require("./Guess");
+const Question = require("./Question");
+const Team = require("./Team");
+
+module.exports = {
+  Answer,
+  Episode,
+  Guess,
+  Question,
+  Team
+};
