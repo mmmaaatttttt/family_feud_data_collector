c62af561f592440257d12a4ac9f25822719f60b1
Matt Lane <lane.matthew@gmail.com>
2018-10-21 18:56:32 -0700

    adding findOrCreate to Team and changing SQL query

--- models/Team.js
+++ models/Team.js
@@ -1,3 +1,6 @@
+const { prompt, choose } = require("promptly");
+const groupBy = require("lodash/groupBy");
+const size = require("lodash/size");
 const db = require("../db");
 
 class Team {
@@ -14,7 +17,7 @@ class Team {
     return new Team(newUser);
   }
 
-  static async get(id) {
+  static async find(id) {
     const result = await db.query(`
       SELECT * FROM teams WHERE id = $1
     `, [id]);
@@ -22,10 +25,38 @@ class Team {
     return new Team(user);
   }
 
-  static async previousEpisodeAirDates(name) {
-    // figure out correct query here
+  static async findByName(name) {
     const result = await db.query(`
-      SELECT e.id, e.air_date 
+      SELECT * FROM teams WHERE name = $1
+    `, [name]);
+    return result.rows.map(row => new Team(row));
+  }
+
+  static async findOrCreate(name) {
+    const results = await Team.previousTeams(name);
+    let choice = "";
+    if (size(results) > 0) {
+      let choiceMsg = `Teams named ${name} already exist. Is one of these teams the one you're referring to?\n`;
+      for (let id of results) {
+        choiceMsg += `${id}. Episodes: ${results[id].air_date.join(", ")}\n`
+      }
+      choiceMsg += "Enter the team's id if you've found it here.\nOtherwise just hit enter, and I'll create a new team for you."
+      choice = await choose(choiceMsg, Object.keys(results).concat(""));
+    }
+    if (choice) {
+      return await Team.find(+choice);
+    }
+    console.log("Okay, I'll create a new team for you.")
+    return await Team.create(name);
+  }
+
+  static async previousTeams(name) {
+    const results = await db.query(`
+      SELECT e.id, e.air_date, 
+      CASE 
+        WHEN t1.name = $1 then t1.id
+        ELSE t2.id
+      END AS team_id
       FROM episodes e
       JOIN teams t1
       ON e.first_team_id = t1.id
@@ -34,7 +65,7 @@ class Team {
       WHERE t1.name = $1
       OR t2.name = $1
     `, [name]);
-    return result.rows;
+    return groupBy(results.rows, "team_id");
   }
 }
 
