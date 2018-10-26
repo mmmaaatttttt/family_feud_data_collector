057f0368df650f815d96cef0d3142bd715fd45cc
Matt Lane <lane.matthew@gmail.com>
2018-10-25 21:56:35 -0700

    updating Guess model

--- models/Guess.js
+++ models/Guess.js
@@ -1,4 +1,33 @@
 class Guess {
+  constructor(obj) {
+    this.id = obj.id;
+    this.question_id = obj.question_id;
+    this.text = obj.text;
+    this.matching_answer_id = obj.matching_answer_id;
+    this.order = obj.order;
+    this.person_id = obj.person_id;
+  }
+
+  static async create(dataObj) {
+    const result = await db.query(`
+      INSERT INTO guesses (
+        question_id,
+        text,
+        matching_answer_id,
+        order,
+        person_id
+      )
+      VALUES ($1, $2, $3, $4, $5) RETURNING *
+    `, [
+      dataObj.question_id,
+      dataObj.text,
+      dataObj.matching_answer_id,
+      dataObj.order,
+      dataObj.person_id
+    ]);
+    let newGuess = result.rows[0];
+    return new Guess(newGuess);
+  }
 }
 
 module.exports = Guess;
