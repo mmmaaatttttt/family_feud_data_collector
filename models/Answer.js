f6b67ad3c857756b133ce85a0ebd33301252c247
Matt Lane <lane.matthew@gmail.com>
2018-10-25 21:56:23 -0700

    updating Answer model

--- models/Answer.js
+++ models/Answer.js
@@ -1,4 +1,27 @@
 class Answer {
+  constructor(obj) {
+    this.id = obj.id;
+    this.question_id = obj.question_id;
+    this.text = obj.text;
+    this.points = obj.points;
+  }
+
+  static async create(dataObj) {
+    const result = await db.query(`
+      INSERT INTO guesses (
+        question_id,
+        text,
+        points
+      )
+      VALUES ($1, $2, $3, $4, $5) RETURNING *
+    `, [
+      dataObj.question_id,
+      dataObj.text,
+      dataObj.points
+    ]);
+    let newAnswer = result.rows[0];
+    return new Answer(newAnswer);
+  }
 }
 
 module.exports = Answer;
